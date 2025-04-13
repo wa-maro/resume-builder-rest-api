5a4d0858671766fe56b54436a12fc0c40e50b362
theomaro <theo.pombe@gmail.com>
2025-04-13 20:54:03 +0300

    define resume model

---
+++ models/Resume.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+// define a Resume schema
+const ResumeSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    title: { type: String, required: true, trim: true },
+    summary: { type: String, required: true, trim: true, default: "" },
+    declaration: {
+      statement: { type: String, required: true, trim: true },
+      signature: { type: String, required: true, trim: true },
+      date: {
+        type: String,
+        required: true,
+        trim: true,
+        validate: {
+          validator: function (v) {
+            return /^([0-2][0-9]|(3)[0-1])\/(0[1-9]|1[0-2])\/\d{4}$/.test(v);
+          },
+          message: (props) =>
+            `${props.value} is not a valid date. Use DD/MM/YYYY format.`,
+        },
+      },
+    },
+  },
+  { timestamps: true }
+);
+
+// define a Resume model
+const Resume = mongoose.model("Resume", ResumeSchema);
+
+export default Resume;
