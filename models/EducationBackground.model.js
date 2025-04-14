fb4ba74fb561202a6516165597f5865b3b52f29b
theomaro <theo.pombe@gmail.com>
2025-04-14 11:05:29 +0300

    define education background model

---
+++ models/EducationBackground.model.js
@@ -0,0 +1,140 @@
+import mongoose from "mongoose";
+
+const educationQualificationsSchema = new mongoose.Schema(
+  {
+    level: {
+      type: String,
+      enum: ["Primary", "O-Level", "A-Level"],
+      required: true,
+    },
+    schoolName: { type: String, required: true, trim: true },
+    startYear: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid 4-digit year`,
+      },
+    },
+    endYear: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid 4-digit year`,
+      },
+    },
+    certificate: {
+      type: String,
+      required: true,
+      enum: [
+        "Primary School Leaving Examination (PSLE)",
+        "The Certificate of Secondary Education Examination (CSEE)",
+        "Advanced Certificate of Secondary Education Examination (ACSEE)",
+      ],
+    },
+    grade: {
+      division: {
+        type: String,
+        trim: true,
+        enum: ["I", "II", "III", "IV", "0"],
+      },
+      points: { type: String },
+    },
+  },
+  {
+    timestamps: true,
+    validate: {
+      validator: function () {
+        return this.startYear <= this.endYear;
+      },
+      message: "Start year must be less than or equal to end year",
+    },
+  }
+);
+
+const professionalQualificationsSchema = new mongoose.Schema(
+  {
+    institutionName: { type: String, required: true, trim: true },
+    qualification: {
+      type: String,
+      required: true,
+      enum: [
+        "Diploma",
+        "Advanced Diploma",
+        "Bachelor's",
+        "Postgraduate Diploma",
+        "Master's",
+        "Doctorate (PhD)",
+      ],
+    },
+    programme: {
+      type: String,
+      trim: true,
+    },
+    startYear: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid 4-digit year`,
+      },
+    },
+    endYear: {
+      type: Number,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return /^\d{4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid 4-digit year`,
+      },
+    },
+    grade: {
+      classification: {
+        type: String,
+        required: true,
+        enum: ["First Class", "Upper Second", "Lower Second", "Pass", "Fail"],
+      },
+      gpa: {
+        type: Number,
+        required: true,
+        min: [0, "GPA cannot be less than 0"],
+        max: [5, "GPA cannot be more than 5"],
+      },
+    },
+  },
+  {
+    timestamps: true,
+    validate: {
+      validator: function () {
+        return this.startYear <= this.endYear;
+      },
+      message: "Start year must be less than or equal to end year",
+    },
+  }
+);
+
+const EducationBackgroundSchema = new mongoose.Schema({
+  resume: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Resume",
+    required: true,
+    unique: true,
+  },
+  educationQualifications: [educationQualificationsSchema],
+  professionalQualifications: [professionalQualificationsSchema],
+});
+
+const EducationBackground = mongoose.model(
+  "EducationBackground",
+  EducationBackgroundSchema
+);
+
+export default EducationBackground;
