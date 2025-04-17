c0ea429c54e41960838ae8c607294d430181721f
theomaro <theo.pombe@gmail.com>
2025-04-17 12:14:42 +0300

    define referee model

---
+++ models/Referee.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const RefereeSchema = new mongoose.Schema(
+  {
+    resume: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Resume",
+      required: true,
+    },
+    name: { type: String, required: true, trim: true },
+    position: { type: String, required: true, trim: true },
+    organization: { type: String, required: true, trim: true },
+    address: { type: String, required: false },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      match: [
+        /^\+?[0-9]{7,15}$/,
+        "Please provide a valid phone number (7-15 digits, optional + at start)",
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+const Referee = mongoose.model("Referee", RefereeSchema);
+
+export default Referee;
