21c601ff8b30e7d4f6080ede9f1db4051c074a16
theomaro <theo.pombe@gmail.com>
2025-04-17 10:23:33 +0300

    define skill model

---
+++ models/Skill.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const SkillSchema = new mongoose.Schema(
+  {
+    resume: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Resume",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["soft", "professional"],
+      required: true,
+    },
+    name: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    proficiency: {
+      type: String,
+      enum: ["beginner", "intermediate", "advanced"],
+    },
+    certification: String,
+  },
+  { timestamps: true }
+);
+
+const Skill = mongoose.model("Skill", SkillSchema);
+
+export default Skill;
